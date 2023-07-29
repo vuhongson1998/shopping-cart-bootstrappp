a6d8b69b37aae3df682705eb5f327b3582d9bb46
sonvh98 <sonvh98os@gmail.com>
2023-07-29 19:26:05 +0700

    Style css

--- src/main.js
+++ src/main.js
@@ -6,20 +6,4 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import "./assets/css/custom.scss";
 
-const clickOutside = {
-    beforeMount: (el, binding) => {
-      el.clickOutsideEvent = event => {
-        // here I check that click was outside the el and his children
-        if (!(el == event.target || el.contains(event.target))) {
-          // and if it did, call method provided in attribute value
-          binding.value();
-        }
-      };
-      document.addEventListener("click", el.clickOutsideEvent);
-    },
-    unmounted: el => {
-      document.removeEventListener("click", el.clickOutsideEvent);
-    },
-  };
-
-createApp(App).use(store).use(router).directive("click-outside", clickOutside).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
